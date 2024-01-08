e5c4b67e58331621b5f0ada2aac966ccae3fd4bc
Mukotso <kelvinmukotso@gmail.com>
2024-01-08 07:03:21 -0800

    ft:add all required modules and configure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt', 
+    '@nuxtjs/tailwindcss',
+     '@sidebase/nuxt-auth'
+    ],
+
+  pinia: {
+    storesDirs: ['./store/**']
+  },
+
+  auth: {
+    baseURL: process.env.API_URL,
+    session: {
+      enableRefreshOnWindowFocus: false,
+      enableRefreshPeriodically: false,
+    },
+    globalAppMiddleware: {
+      isEnabled: true
+    },
+    provider: {
+      type: 'local',
+      pages: {
+        login: "/auth/login",
+      },
+      endpoints: {
+        signIn: { path: "/login", method: 'post' }, //get token
+        signOut: { path: "", method: "get" },
+        getSession: { path: "/refresh-token" }
+      },
+      token: {
+        signInResponseTokenPointer: '/token',
+        maxAgeInSeconds: 60 * 60 * 24 * 2
+      },
+
+    },
+  },
+
+
 })
