48882530af26dc07d6c484fd6b3fdbee917744a8
Mukotso <kelvinmukotso@gmail.com>
2024-01-10 07:00:24 -0800

    ft install primeVue, primeIcons, tailwinds, nuxt, vee-validate, vee-validate rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,39 +4,63 @@ export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt', 
     '@nuxtjs/tailwindcss',
-     '@sidebase/nuxt-auth'
+    //  '@sidebase/nuxt-auth',
+    '@vee-validate/nuxt',
     ],
 
   pinia: {
     storesDirs: ['./store/**']
   },
 
-  auth: {
-    baseURL: process.env.API_URL,
-    session: {
-      enableRefreshOnWindowFocus: false,
-      enableRefreshPeriodically: false,
-    },
-    globalAppMiddleware: {
-      isEnabled: true
-    },
-    provider: {
-      type: 'local',
-      pages: {
-        login: "/auth/login",
-      },
-      endpoints: {
-        signIn: { path: "/login", method: 'post' }, //get token
-        signOut: { path: "", method: "get" },
-        getSession: { path: "/refresh-token" }
-      },
-      token: {
-        signInResponseTokenPointer: '/token',
-        maxAgeInSeconds: 60 * 60 * 24 * 2
-      },
+  plugins: [
+    '~/plugins/vee-validate.js',
+  ],
 
+  css: [
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
+    "primeflex/primeflex.css",
+    "@/assets/styles/global.css"
+  ],
+
+  build: {
+    transpile: ["primevue"],
+  },
+  app: {
+    pageTransition: { name: "fade", mode: "out-in" },
+    layoutTransition: {
+      name: "slide",
+      mode: "out-in", // default
     },
   },
+
+  // auth: {
+  //   baseURL: process.env.API_URL,
+  //   session: {
+  //     enableRefreshOnWindowFocus: false,
+  //     enableRefreshPeriodically: false,
+  //   },
+  //   globalAppMiddleware: {
+  //     isEnabled: true
+  //   },
+  //   provider: {
+  //     type: 'local',
+  //     pages: {
+  //       login: "/auth/login",
+  //     },
+  //     endpoints: {
+  //       signIn: { path: "/login", method: 'post' }, //get token
+  //       signOut: { path: "", method: "get" },
+  //       getSession: { path: "/refresh-token" }
+  //     },
+  //     token: {
+  //       signInResponseTokenPointer: '/token',
+  //       maxAgeInSeconds: 60 * 60 * 24 * 2
+  //     },
+
+  //   },
+  // },
 
 
 })
